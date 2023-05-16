8518256fecdd3c9505bd565f31bbbf4f94f2b1fb
arthurio <kebabchik13@gmail.com>
2023-05-16 15:26:37 +0500

    php syntax in template changed to alternative syntax

--- local/templates/.default/components/bitrix/news.list/link/template.php
+++ local/templates/.default/components/bitrix/news.list/link/template.php
@@ -5,14 +5,20 @@
 
 <div id="barba-wrapper">
     <div class="article-list">
-        <?php foreach ($arResult['ITEMS'] as $arItem) { ?>
+        <? foreach ($arResult['ITEMS'] as $arItem): ?>
             <a class="article-item article-list__item" href="<?= $arItem['DISPLAY_PROPERTIES']['PROS_HREF']['VALUE'] ?>" data-anim="anim-3">
-                <div class="article-item__background"><img src="<?= $arItem['DISPLAY_PROPERTIES']['PROS_IMG']['VALUE'] ?>" data-src="<?= $arItem['DISPLAY_PROPERTIES']['PROS_IMG_DATA']['VALUE'] ?>" alt="" /></div>
+                <div class="article-item__background">
+                    <img src="<?= $arItem['DISPLAY_PROPERTIES']['PROS_IMG']['VALUE'] ?>" data-src="<?= $arItem['DISPLAY_PROPERTIES']['PROS_IMG_DATA']['VALUE'] ?>" alt="" />
+                </div>
                 <div class="article-item__wrapper">
-                    <div class="article-item__title"><?= $arItem['DISPLAY_PROPERTIES']['PROS_TITLE']['VALUE'] ?></div>
-                    <div class="article-item__content"><?= $arItem['DISPLAY_PROPERTIES']['PROS_DESC']['VALUE'] ?></div>
+                    <div class="article-item__title">
+                        <?= $arItem['DISPLAY_PROPERTIES']['PROS_TITLE']['VALUE'] ?>
+                    </div>
+                    <div class="article-item__content">
+                        <?= $arItem['DISPLAY_PROPERTIES']['PROS_DESC']['VALUE'] ?>
+                    </div>
                 </div>
             </a>
-        <?php } ?>
+        <? endforeach; ?>
     </div>
 </div>
